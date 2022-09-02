8db0e6f98001ce757d63daeba80cc341fe56f76a
Bintang Alsyahadat <92221902+BintangAl@users.noreply.github.com>
2022-09-02 17:42:14 +0700

    update ApiController.php

--- app/Http/Controllers/API/ApiController.php
+++ app/Http/Controllers/API/ApiController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Models\Address;
 use Illuminate\Http\Request;
+use App\Models\Product;
+use App\Models\Transaction;
 
 use function PHPUnit\Framework\returnSelf;
 
@@ -59,5 +61,138 @@ class ApiController extends Controller
         } else {
             return response(['Api key not found.'], 404);
         }
+    }
+   
+   public function Users()
+   {
+     return response()->json([
+       'status' => 200,
+       'message' => 'Success Get Data.',
+       'data' => User::get(['name', 'email'])
+     ]); 
+   }
+    
+    public function Products(Request $request)
+    {
+      if($request->key == config('services.app_api.key')) {
+        if($request->action == 'list') {
+          return response()->json([
+            'status' => 200,
+            'message' => 'Success Get Data',
+            'data' => Product::all()
+          ], 200);
+        } elseif ($request->action == 'create') {
+          //dd($request->all());
+          $validate = $request->validate([
+            'id' => 'required',
+            'product_name' => 'required',
+            'product_price' => 'required',
+            'product_detail' => 'required',
+            'product_image' => 'required',
+            'product_stok' => 'required',
+            'product_sold' => 'required',
+            'category_id' => 'required'
+          ]);
+          
+          Product::create($validate);
+          return response()->json([
+            'status' => 200,
+            'message' => 'Product Created.'
+            'data' => $validate
+           ], 200);
+          
+        } elseif ($request->action == 'update') {
+          //dd($request->all());
+          $validate = $request->validate([
+            'id' => 'required',
+            'product_name' => 'required',
+            'product_price' => 'required',
+            'product_detail' => 'required',
+            'product_image' => 'required',
+            'product_stok' => 'required',
+            'product_sold' => 'required',
+            'category_id' => 'required'
+          ]);
+          
+          Product::where('id, $id')->update($validate);
+          return response()->json([
+            'status' => 200,
+            'message' => 'Product Updated.'
+            'data' => $validate
+           ], 200);
+          
+        } elseif ($request->action == 'delete') 
+          
+          if(isset($request->id)) {
+            $product = Product::where('id', $request->id);
+            
+            if (count($product->get())) {
+              $product->delete();
+              return response()->json([
+                'status' => 200
+                'message' => 'Product Deleted.',
+                'data' => []
+              ], 200)
+            } else {
+              return response()->json([
+                'status' => 404,
+                'message' => 'ID not found.',
+                'data' => []
+              ], 404)
+            }
+          } else {
+            return response()->json([
+              'status' => 404,
+              'message' => 'ID not found.',
+              'data' => []
+            ], 404)
+          }
+        } else {
+          return response()->json([
+            'status' => 404,
+            'message' => 'action not found.'
+            'data' => []
+          ], 404);
+        }
+      } else {
+          return response()->json([
+            'status' => 404,
+            'message' => 'key not found.'
+            'data' => []
+          ], 404);
+       }
+    }
+   
+    public function Transactions(Request $request)
+    {
+      if ($request->key == config('services.app_api.key')) {
+        if ($request->action == "list") {
+          return response()->json([
+            'status' => 200,
+            'message' => 'Success Get Data',
+            'data' => Transaction::all()
+          ], 200);
+        } elseif ($request->action == 'update') {
+          $validate = $request->validate([
+            'id' => 'required',
+            'no_resi' => 'required',
+            'status' => 'required'
+          ]);
+          
+          Transaction::where('id', $request->id)->update($validate);
+          
+          return response()->json([
+            'status' => 200,
+            'message' => 'Transactions Updated.',
+            'data' => $validate
+          ], 200);
+        }
+      } else {
+          return response()->json([
+            'status' => 404,
+            'message' => 'key not found.'
+            'data' => []
+          ], 404);
+      }
     }
 }
